dd8b352030155e5d091da2ea7958e329192e9ded
Papai Biswas <84518377+liton12345@users.noreply.github.com>
2023-06-18 09:22:52 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,27 @@
+ import java.util.*;
+
+
+public class main {
+    public static void  main(String[] args) {
+
+
+
+
+        Scanner sc = new Scanner(System.in);
+                int inp1 = sc.nextInt();
+                //int inp2 = sc.nextInt();
+
+        for( int i = 1; i <= inp1 ; i++ ) {
+
+            int q = i * inp1;
+
+            System.out.println( inp1 + " * " + i + " = " + q);
+
+
+        }
+
+
+
+
+    }
+  }
